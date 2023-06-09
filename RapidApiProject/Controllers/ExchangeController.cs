5756510034cfd873ed75f2fc3e22718cf1a83c26
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-06-09 17:47:13 +0300

    Add project files.

---
+++ RapidApiProject/Controllers/ExchangeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RapidApiProject.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://currency-exchange.p.rapidapi.com/exchange?from=USDT&to=TRY&q=1.0"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "baec683ea0msh79a13f8005f2b44p1d4f7cjsnb5bfc71b239e" },
+        { "X-RapidAPI-Host", "currency-exchange.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                ViewBag.v = body;
+
+            }
+            return View();
+
+        }
+    }
+}
+
+/*Kur başlıkları dropdown'a enum liste üzerinden aktarılsın 
+             1.dd neyden
+             2.dd neye
+            seçilen kurlara göre kur bilgisi getiren uygulamayı yazın*/
+
+
+
